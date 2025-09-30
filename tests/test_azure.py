4297cff0b9100ba1d80c8ff519a3b76901932993
Lukas Hennies <lukas.hennies@anticipate.ml>
2025-09-30 11:31:05 +0200

    chore: ruff

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -303,9 +303,7 @@ class AzureStorageTest(TestCase):
             client_mock = mock.MagicMock()
             bsc_mocked.return_value.get_container_client.return_value = client_mock
             self.assertEqual(storage.client, client_mock)
-            bsc_mocked.assert_called_once_with(
-                "https://bar.com", credential="foo_cred"
-            )
+            bsc_mocked.assert_called_once_with("https://bar.com", credential="foo_cred")
 
     def test_connection_string_can_have_missing(self):
         storage = azure_storage.AzureStorage(
